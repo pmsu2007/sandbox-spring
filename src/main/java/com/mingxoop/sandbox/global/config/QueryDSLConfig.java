777dd6606feb8fa315203370c52afbb9b0346536
pmsu2007 <pmsu2007@naver.com>
2025-06-13 01:09:28 +0900

    fix : QueryDSL의 기본 실행 방식을 JPA 표준으로 변경하여 Hibernate 내부 API 호출회피

--- src/main/java/com/mingxoop/sandbox/global/config/QueryDSLConfig.java
+++ src/main/java/com/mingxoop/sandbox/global/config/QueryDSLConfig.java
@@ -1,5 +1,6 @@
 package com.mingxoop.sandbox.global.config;
 
+import com.querydsl.jpa.JPQLTemplates;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
@@ -13,6 +14,9 @@ public class QueryDSLConfig {
 
 	@Bean
 	public JPAQueryFactory jpaQueryFactory() {
-		return new JPAQueryFactory(entityManager);
+		return new JPAQueryFactory(
+				JPQLTemplates.DEFAULT,
+				entityManager
+		);
 	}
 }
